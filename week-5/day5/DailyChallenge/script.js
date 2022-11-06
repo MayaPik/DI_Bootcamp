18d4a3b526da97c066d139a6e75fad608823055f
Maya Pik <maya.pik@gmail.com>
2022-11-06 18:59:13 +0200

    end-of-week-5

--- week-5/day5/DailyChallenge/script.js
+++ week-5/day5/DailyChallenge/script.js
@@ -10,20 +10,25 @@ form.addEventListener('submit', addTask);
 
 
 function addTask(event) {
+    event.preventDefault();
+
     if (add.value !== "") {
         let done = false
         let task_id = i
         let text =add.value;
        tasks.push({task_id, text, done});
 
-       let x = document.createElement('button')
-       x.addEventListener('click', deleteTask);
+       let x = document.createElement('input')
+       x.setAttribute('type', 'button')
+       x.setAttribute('data-task-id', i)
 
         let xFont = document.createElement('i')
         xFont.setAttribute('class', 'fa-solid fa-x');
         x.appendChild(xFont)
 
        let newOne = document.createElement('div')
+       newOne.setAttribute('data-task-id', i)
+
        newOne.appendChild(x)
        x.style.marginRight = '20px'
 
@@ -40,21 +45,29 @@ function addTask(event) {
 
 
     }
-    event.preventDefault();
     
 }
 
-list.addEventListener('click', event => {
-    let number = event.target.dataset.taskId;
-    if (tasks[number].task_id == number) {
-        tasks[number].done = !tasks[number].done ;
-        list.getElementsByTagName('div')[number].classList.toggle('todo-complete');
-    } 
-   
-});
+    list.addEventListener('click', event => {
+        let number = event.target.dataset.taskId;
+        if (event.target.type === 'checkbox') {
+            if (tasks[number].task_id == number) {
+                tasks[number].done = !tasks[number].done ;
+                list.getElementsByTagName('div')[number].classList.toggle('todo-complete');
+            }
+        } else if(event.target.type === 'button') {
+            deleteTask(number);
+        }
+    });
 
 //Create a function named deleteTask(), that as soon as the user clicks on the “X” button, delete that specific task from the array listTasks.
 
-function deleteTask(event) {
-    console.log(event)
+function deleteTask(number) {
+    let taskToDelete = document.querySelector(`div[data-task-id="${number}"]`);
+    taskToDelete.remove();
+    for (let j=0; j< tasks.length; j++) {
+    if (tasks[j].task_id == number) {
+    tasks.splice(j,1);
+    }
+}
     }
