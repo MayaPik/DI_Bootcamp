61bdfece2de73b825fef2a06717f01eede359164
Maya Pik <maya.pik@gmail.com>
2022-11-15 21:22:06 +0200

    i need to get rid of prompt

--- week-6/Hackaton/script.js
+++ week-6/Hackaton/script.js
@@ -1,13 +1,6 @@
-function getValue() {
-  let marker = myForm.elements.story
-  consol.log(maker)
-}
 let markers = []; //The markers
 var infowindow;
 var map;
-let myForm = document.getElementById('form1')
-myForm.style.display = 'none'
-
   
 function getInit() {
   const map = new google.maps.Map(
@@ -45,14 +38,18 @@ function getInit() {
     }
     
     const bounds = new google.maps.LatLngBounds();
-
+      var popup = document.getElementById("myPopup");
+      popup.classList.add("show");
+    
 
     places.forEach((place) => {
 
       google.maps.event.addListener(map, "dblclick", (event) => {
         addMarker(event.latLng, map);
+        popup.innerHTML = "Click on the Marker to add your story"
 
       });
+      
 
 
       function addMarker(location, map) {
@@ -64,9 +61,8 @@ function getInit() {
           scaledSize: new google.maps.Size(30, 30),
 
         };
-         
-          
 
+         
         // Create a marker for each place.
         markers.push(
           new google.maps.Marker({
@@ -88,8 +84,24 @@ function getInit() {
         
         marker.addListener("click", () => {
           if (marker.description == 0 ) {
+            popup.classList.remove("show");
+
           marker.description = prompt('Write here your story/ Experience!',)
           } 
+
+  let stories = document.getElementById('stories')
+
+
+    let box = document.createElement('div')
+    box.setAttribute('class', 'card')
+    let title = document.createElement('h2')
+    marker.position
+    title.innerHTML = new google.maps.LatLng(43,2)
+    let description = document.createElement('h3')
+    description.innerHTML = marker.description;
+    box.appendChild(title)
+    box.appendChild(description)
+    stories.appendChild(box)
           infoWindow.close();
           infoWindow.open(marker.getMap(), marker);
           infoWindow.setContent(marker.description);
@@ -117,21 +129,30 @@ function getInit() {
     map.fitBounds(bounds);
   });
 
+
+  let button1 = document.getElementById('button1')
+  button1.addEventListener('click', seeStories)
+
+  let button2 = document.getElementById('button2')
+  button2.addEventListener('click', closeStories)
+
+    
+  function seeStories() {
+    let map = document.getElementById('map');
+    map.style.height = '60%'
+    let stories = document.getElementById('stories')
+    stories.style.display= 'flex'
+
+    
+}
+function closeStories() {
+  let map = document.getElementById('map');
+  map.style.height = '85%'
+  let stories = document.getElementById('stories')
+  stories.style.display= 'none'
+
+  
 }
 
-let button = document.getElementById('button')
-button.addEventListener('click', seeStories)
-
-function seeStories() {
-  for (i=0 ; i < markers.length ; i++) {
-    let box = document.createElement('div')
-    let title = document.createElement('h1')
-    title.innerHTML = markers[i].title;
-    let description = document.createElement('h3')
-    description.innerHTML = markers[i].description;
-    box.appendChild(title)
-    box.appendChild(description)
-    document.body.appendChild(box)
-}
 }
 window.getInit = getInit;
