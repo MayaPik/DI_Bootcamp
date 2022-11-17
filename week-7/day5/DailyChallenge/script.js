73a56c65f21e6103b51f299d0030f41282ccf772
Maya Pik <maya.pik@gmail.com>
2022-11-17 17:26:00 +0200

    end of week-7

--- week-7/day5/DailyChallenge/script.js
+++ week-7/day5/DailyChallenge/script.js
@@ -1,35 +1,12 @@
-let client = "John";
+let arr = []
 
-const groceries = {
-    fruits : ["pear", "apple", "banana"],
-    vegetables: ["tomatoes", "cucumber", "salad"],
-    totalPrice : "20$",
-    other : {
-        payed : true,
-        meansOfPayment : ["cash", "creditCard"]
+function allTruthy(a, b, c) {
+    for (i=0; i< arguments.length; i++) {
+arr.push(arguments[i])
     }
+    console.log(arr.every(element => element == true)); 
+
+
 }
 
-//Create an arrow function named displayGroceries, that console.logs the 3 fruits from the groceries object. Use the forEach method.
-
-const displayGroceries = () => groceries.fruits.forEach((fruit) => console.log(fruit));
-displayGroceries()
-
-//Create another arrow function named cloneGroceries
-//In the function, create a variable named user that is a copy of the client variable. 
-//(Tip : make the user variable equal to the client variable)
-
-//In the function, create a variable named shopping that is equal to the groceries variable.
-//Change the value of the totalPrice key to 35$. Will we also see this modification in the shopping object ? Why ?
-//Change the value of the payed key to false. Will we also see this modification in the shopping object ? Why ?
-
-const cloneGroceries = () => { 
-    let user = client; 
-    client = 'bety';
-    console.log(user); //not changed- reference by value
-    let shopping = groceries;
-    groceries.totalPrice = '35$'
-    groceries.other.payed = false;
-    console.log(shopping) //changed- because it is begin refer to the same place
-}
-cloneGroceries()
+allTruthy(true, true, true, true, true)
--- week-7/day5/MiniProject/script.js
+++ week-7/day5/MiniProject/script.js
@@ -29,9 +29,17 @@ const quotes = [
     author: "Emma Goldman",
     quote: "The most violent element in society is ignorance."
   },
+  {
+    id: 6,
+    author: "George Orwell",
+    quote: "If liberty means anything at all, it means the right to tell people what they do not want to hear"
+  },
 
 ]
 
+quotes.forEach(object => {
+  object.likes = 0;
+});
 //When the “Generate Quote” button is pressed, retrieve randomly a quote (ie. an object), and display it in the DOM - like the image above.
 // When clicking on the button, make sure you don’t display the same quote twice in a row.
 let lastQuote = -1;
@@ -43,9 +51,22 @@ space.appendChild(quoteSpot)
 let authorSpot = document.createElement('h3')
 space.appendChild(authorSpot)
 
-const button = document.getElementById('generate')
+const mainButton = document.getElementById('generate')
+const buttonWithSpaces = document.getElementById('charactersWithSpaces')
+const buttonWithoutSpaces = document.getElementById('charactersNoSpaces')
+const buttonNumOfWords = document.getElementById('numberOfWords')
+const likeButton = document.getElementById('like')
+
+
+let wordsResult = document.getElementById('wordsResult')
+let noSpacesResult = document.getElementById('noSpacesResult')
+let spacesResult = document.getElementById('spacesResult')
+let likesResult = document.getElementById('likesResult')
+
+
 
 const generator = () => {
+
     let randomNumber  = (Math.round(Math.random() * (quotes.length-1))) 
     if(lastQuote === randomNumber) {
       generator();
@@ -54,15 +75,54 @@ const generator = () => {
       let randomQuote = quotes[randomNumber]
       quoteSpot.innerText = randomQuote.quote
       authorSpot.innerText = randomQuote.author
+      spacesResult.innerText = ""
+      noSpacesResult.innerText = ""
+      wordsResult.innerText = ""
+      likesResult.innerText = 0;
+      likesResult.value = randomQuote.likes 
+
   }
 }
 
-button.addEventListener('click', generator)
+mainButton.addEventListener('click', generator)
+
+
+//A button that gives the number of character inside each quote (space included)
+buttonWithSpaces.addEventListener('click', withSpaces)
+
+function withSpaces() {
+  spacesResult.innerText = ( quoteSpot.innerHTML.length )
+}
+
+//A button that gives the number of character inside each quote (space NOT included)
+
+buttonWithoutSpaces.addEventListener('click', withOutSpaces)
+
+function withOutSpaces() {
+  noSpacesResult.innerText = (quoteSpot.innerHTML.replace(/\s/g, '').length )
+}
+
+//A button that gives the number of words in each quote
+
+
+buttonNumOfWords.addEventListener('click', numOfWords)
+function numOfWords() {
+  wordsResult.innerText =  (quoteSpot.innerHTML.split(" ").length)
+}  
+
+//A button “Like” for the user to like a quote. Hint : add a new key to each object that will represent the number of “likes”.
+
+
+likeButton.addEventListener('click', toLike)
+function toLike() {
+  likesResult.value ++;
+  likesResult.innerText = likesResult.value
+}  
+
 
 let addingForm = document.getElementById('addQuote')
 let quoteInput = document.getElementById('quote')
 let authorInput = document.getElementById('author')
-
 
 const addNewOne = (e) => {
   e.preventDefault()
@@ -75,3 +135,35 @@ const addNewOne = (e) => {
 }
 
 addingForm.addEventListener('submit', addNewOne);
+
+let searchForm = document.getElementById('search')
+let nextButton = document.getElementById('next')
+let previousButton = document.getElementById('previous')
+
+
+searchForm.addEventListener('submit', byAuthor)
+
+let nameOfAuthor = document.getElementById('name')
+let place = document.getElementById('quotesbyauthor')
+function byAuthor(e) {
+  let n = 0;
+  const spesific = quotes.filter(each => each.author == nameOfAuthor.value);
+    let newDisplay = document.createElement('div')
+    newDisplay.innerHTML = spesific[n].quote;
+    console.log(spesific.length)
+    place.appendChild(newDisplay)
+    nextButton.addEventListener('click', function() {
+      if (n < spesific.length-1) {
+      n +=1;
+      newDisplay.innerHTML = spesific[n].quote;
+      }
+      
+    })
+    previousButton.addEventListener('click', function() {
+      if (n > 0) {
+      n-=1
+      newDisplay.innerHTML = spesific[n].quote;
+      }
+    })
+  e.preventDefault()
+}
