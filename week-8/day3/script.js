810dfa15708a5fc5b2484276b7277f51fb74d859
Maya Pik <maya.pik@gmail.com>
2022-11-22 20:05:24 +0200

    need to keep this active

--- week-8/day3/script.js
+++ week-8/day3/script.js
@@ -1,134 +1,40 @@
-function say(name) {
-  alert("Hello " + name);
-}         
-
-say("Sarah"); 
-
-(function (name) {
-  alert("hello" + name);
-})("sarah")
-
-const hummus = function(factor) {
-  const ingredient = function(amount, unit, name) {
-      let ingredientAmount = amount * factor;
-      if (ingredientAmount > 1) {
-          unit += "s";
-      }
-      console.log(`${ingredientAmount} ${unit} ${name}`);
-  };
-  ingredient(1, "can", "chickpeas");
-  ingredient(0.25, "cup", "tahini");
-  ingredient(0.25, "cup", "lemon juice");
-  ingredient(1, "clove", "garlic");
-  ingredient(2, "tablespoon", "olive oil");
-  ingredient(0.5, "teaspoon", "cumin");
+var employee = {    // Object we want to destructure
+  firstname: 'Jon',
+  lastname: 'Snow',
+  dateofbirth: '1990'
 };
 
-hummus(2)
+// Destructuring the object into variables without 
+// assigning default values 
+var { firstname, lastname, country } = employee;
+console.log("Without setting default values")
+console.log( firstname, lastname, country);
 
-function add() { //parent function
-  let counter = 0;
-  function plus() {
-      counter += 1;
-      console.log(counter) ;
+// Destructuring the object into variables by 
+// assigning default values 
+var { firstname = 'default firstname', 
+    lastname = 'default lastname', 
+    country = 'default country' } = employee;
+console.log("\n After setting default values")
+console.log( firstname, lastname, country);
+
+
+console.log(Object.entries(employee));
+
+let myObj = {
+  name : "John",
+  lastName : "Doe",
+  age : 25,
+  friends : ["Mark", "Lucie", "Ana"]
+}
+
+//Count how many keys and values are in the object below
+
+console.log(`There are ${(Object.keys(myObj).length)} keys, and ${(Object.values(myObj).length)} values`);
+
+let numof = (Object.entries(myObj).length)
+let i=0 
+ for (let [key, value] of Object.entries(myObj)) {
+    console.log(`the ${i} keys is ${key} and the ${i} value is ${value}`);
+    i++
   }
-  plus();  //console log 1
-}
-
-add() //console log 1
-
-
-
-function outside(x) {
-  function inside(y) {
-    return x + y;
-  }
-  return inside;
-}
-
-console.log(outside(3)(5)); // returns 8
-
-
-function newAdd() {
-  let counter = 0;
-    function calculus() {
-    counter += 1; 
-    return counter
-    }
-    console.log(calculus());
-}
-
-newAdd();
-
-const add2 = a => b => a + b;
-const result = add2(2)(3)
-
-const multiply = (n, m) => (n * m);
-const curryedMultiply = (n) => (m) => multiply(n, m)
-let triple = curryedMultiply(3)
-triple(4)
-
-const g = n => n + 1;
-const f = n => n * 2;
-
-const h = x => f(g(x)); //2*(21)
-
-h(20);  //42
-
-let myCar = new Object();
-myCar.make = 'Ford';
-myCar.model = 'Mustang';
-myCar.year = 1969;
-
-console.log(myCar.color); // undefined
-console.log(myCar['year']); // 1969
-console.log(myCar.year); // 1969
-
-let myObj = new Object(),
-    str = 'myString',
-    rand = Math.random(),
-    obj = new Object();
-
-myObj.type              = 'Dot syntax';
-myObj['date created']   = 'String with space';
-myObj[str]              = 'String value';
-myObj[rand]             = 'Random Number';
-myObj[obj]              = 'Object';
-myObj['']               = 'Even an empty string';
-
-console.log(myObj)
-// output}
-
-
-let user = makeUser("John", 30);
-alert(user.name + user.age); // John
-
-// The same as
-function makeUser(name, age) {
-  return {
-    name, // same as name: name
-    age,  // same as age: age
-    // ...
-  };
-}
-
-function showProps(obj, objName) {
-  let result = '';
-  for (let i in obj) {
-    if (obj.hasOwnProperty(i)) {
-      result += `${objName}.${i} = ${obj[i]}`;
-    }
-  }
-  return result;
-}
-
-showProps(myCar, "myCar")
-
-const person = {
-  name: 'Lydia',
-  age: 21,
-};
-
-for (const item in person) {
-  console.log(item);
-}
--- week-8/day4/ExercisesXP/script.js
+++ week-8/day4/ExercisesXP/script.js
@@ -1,78 +1,64 @@
-//Exercise 1 : Find The Sum
-//Create a one line function (ie. an arrow function) that receives two numbers as parameters and returns the sum.
+//🌟 Exercise 1 : Location
 
-let sum = (a, b) => (a + b)
-
-
-
-//Exercise 2 : Kg And Grams
-//Create a function that receives a weight in kilograms and returns it in grams. (Hint: 1 kg is 1000gr)
-
-function weightCalc(kilo){
-    console.log(kilo*1000)
-};
-  weightCalc(32);
-
-let weightCalc2 = function(kilo) {
-  console.log(kilo*1000)
-}(33);
-
-//this is an anonymus function - you can't do : weightCalc2(32);
-
-
-let weightCalc3 = (kilo) => (kilo*1000)
-console.log(weightCalc3(23))
-
-//Exercise 3 : Fortune Teller
-//Create a self invoking function that takes 4 arguments: number of children, partner’s name, geographic location, job title.
-
-let sentenceFunction = function(num, name, location, title) {
-  let newOne = document.createElement('div');
-  newOne.innerHTML = `You will be a ${title} in ${location}, and married to ${name} with ${num} kids.`
-  document.body.appendChild(newOne)
-}(23, 'Orlo', 'Tel Aviv', 'Police officer');
-
-
-//Exercise 4 : Welcome
-//John has just signed in to your website and you want to welcome him.
-
-const user = {
-  name: "John",
-  imgUrl: 'http://i.stack.imgur.com/KUvWS.jpg'
+const person = {
+  name: 'John Doe',
+  age: 25,
+  location: {
+      country: 'Canada',
+      city: 'Vancouver',
+      coordinates: [49.2827, -123.1207]
+  }
 }
 
-let welcome = function(name){
-  let newOne = document.createElement('div');
-  newOne.innerHTML = name;
-  newOne.setAttribute('class','navbar-brand text-white')
-  let img = document.createElement('img');
-  img.src = user.imgUrl;
-  img.style.width = '50px'
-  img.style.height = '50px'
-  img.style.borderRadius = '5px'
-  let nav = document.getElementById('container')
+const {name, location: {country, city, coordinates: [lat, lng]}} = person;
 
-  nav.prepend(img);
-  nav.prepend(newOne)
-}(user.name);
+//I am John Doe from Vancouver, Canada. Latitude(49.2827), Longitude(-123.1207)
 
-//Exercise 5 : Juice Bar
+console.log(`I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`);
 
-const makeJuice = function(size) {
-  let ingredients = [];
-  const addIngredients = function(first, second, third) {
-      ingredients.push(first ,second ,third )
-      }
-      addIngredients('banana ', 'milk ', 'sugar ')
-      addIngredients('orange ','apples ', 'strawberry ')
 
-  const displayJuice = function() { 
-    let newOne = document.createElement('div');
-    newOne.innerHTML = `The client wants a ${size} juice, containing ${ingredients}.`
-    document.body.appendChild(newOne)
+//🌟 Exercise 2: Display Student Info
 
-    }
-    displayJuice()
-};
+function displayStudentInfo(objUser){
+  const {first, last} = objUser
+  return `your full name is ${first} ${last}`
+  //destructuring
+}
 
-makeJuice('small')
+displayStudentInfo({first: 'Elie', last:'Schoppik'});
+
+
+//Using the code above, destructure the parameter inside the function and return a string as the example seen below:
+//output : 'Your full name is Elie Schoppik'
+
+//🌟 Exercise 3: User & Id
+
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+
+//Excepted output: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+//FYI : The number is their ID number.
+function usersToArray(obj) {
+  return Object.entries(obj)
+}
+
+usersToArray(users)
+//Modify the outcome of part 1, by multipling the user’s ID by 2.
+//Excepted output: [ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
+
+function usersToDoubleArray(obj) {
+
+  return Object.fromEntries(
+    Object.entries(obj).map(([ key, val ]) => [ key, val * 2 ])
+)}
+usersToDoubleArray(users)
+
+//Exercise 4 : Person Class
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const member = new Person('John');
+console.log(typeof member);
+// object - person {name: John}
