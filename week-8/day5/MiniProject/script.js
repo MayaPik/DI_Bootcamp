a24107f3338b9f648854ff4312c007bdad9f5202
Maya Pik <maya.pik@gmail.com>
2022-11-24 09:30:03 +0200

    miniproject1

--- week-8/day5/MiniProject/script.js
+++ week-8/day5/MiniProject/script.js
@@ -1,169 +1,132 @@
-const quotes = [
-  {
-    id: 0,
-    author: "Emma Goldman",
-    quote: "People have only as much liberty as they have the intelligence to want and the courage to take."
-  },
+const robots = [
   {
     id: 1,
-    author: "Tacitus",
-    quote: "If you would know who controls you see who you may not criticise."
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    image: 'https://robohash.org/1?200x200'
   },
   {
     id: 2,
-    author: "Cormac McCarthy",
-    quote: "At one time in the world there were woods that no one owned"
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv',
+    image: 'https://robohash.org/2?200x200'
   },
   {
     id: 3,
-    author: "Lysander Spooner",
-    quote: "A man is no less a slave because he is allowed to choose a new master once in a term of years."
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net',
+    image: 'https://robohash.org/3?200x200'
   },
   {
     id: 4,
-    author: "George Orwell",
-    quote: "If I had understood the situation a bit better I should probably have joined the Anarchists."
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org',
+    image: 'https://robohash.org/4?200x200'
   },
   {
     id: 5,
-    author: "Emma Goldman",
-    quote: "The most violent element in society is ignorance."
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca',
+    image: 'https://robohash.org/5?200x200'
   },
   {
     id: 6,
-    author: "George Orwell",
-    quote: "If liberty means anything at all, it means the right to tell people what they do not want to hear"
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info',
+    image: 'https://robohash.org/6?200x200'
   },
-
-]
-
-quotes.forEach(object => {
-  object.likes = 0;
-});
-//When the “Generate Quote” button is pressed, retrieve randomly a quote (ie. an object), and display it in the DOM - like the image above.
-// When clicking on the button, make sure you don’t display the same quote twice in a row.
-let lastQuote = -1;
-
-const space = document.getElementById('quotes')
-
-let quoteSpot = document.createElement('h1')
-space.appendChild(quoteSpot)
-let authorSpot = document.createElement('h3')
-space.appendChild(authorSpot)
-
-const mainButton = document.getElementById('generate')
-const buttonWithSpaces = document.getElementById('charactersWithSpaces')
-const buttonWithoutSpaces = document.getElementById('charactersNoSpaces')
-const buttonNumOfWords = document.getElementById('numberOfWords')
-const likeButton = document.getElementById('like')
-
-
-let wordsResult = document.getElementById('wordsResult')
-let noSpacesResult = document.getElementById('noSpacesResult')
-let spacesResult = document.getElementById('spacesResult')
-let likesResult = document.getElementById('likesResult')
-
-
-
-const generator = () => {
-
-    let randomNumber  = (Math.round(Math.random() * (quotes.length-1))) 
-    if(lastQuote === randomNumber) {
-      generator();
-  } else {
-      lastQuote = randomNumber;
-      let randomQuote = quotes[randomNumber]
-      quoteSpot.innerText = randomQuote.quote
-      authorSpot.innerText = randomQuote.author
-      spacesResult.innerText = ""
-      noSpacesResult.innerText = ""
-      wordsResult.innerText = ""
-      likesResult.innerText = 0;
-      likesResult.value = randomQuote.likes 
-
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz',
+    image: 'https://robohash.org/7?200x200'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me',
+    image: 'https://robohash.org/8?200x200'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io',
+    image:'https://robohash.org/9?200x200'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz',
+    image:'https://robohash.org/10?200x200'
   }
-}
-
-mainButton.addEventListener('click', generator)
+  ];
 
 
-//A button that gives the number of character inside each quote (space included)
-buttonWithSpaces.addEventListener('click', withSpaces)
+  let space = document.getElementById('place');
+  let allRobots = []
 
-function withSpaces() {
-  spacesResult.innerText = ( quoteSpot.innerHTML.length )
-}
+  class Robot {
+    constructor(id,name,username,email,image) {
+      this.id = id,
+      this.name = name,
+      this.username = username,
+      this.email = email,
+      this.image = image
+    }
+    createRobotCard() {
+      this.robotCard = document.createElement('div')
+      this.robotCard.className = "card col-lg-3 col-md-4 col-sm-4 m-2";
 
-//A button that gives the number of character inside each quote (space NOT included)
-
-buttonWithoutSpaces.addEventListener('click', withOutSpaces)
-
-function withOutSpaces() {
-  noSpacesResult.innerText = (quoteSpot.innerHTML.replace(/\s/g, '').length )
-}
-
-//A button that gives the number of words in each quote
-
-
-buttonNumOfWords.addEventListener('click', numOfWords)
-function numOfWords() {
-  wordsResult.innerText =  (quoteSpot.innerHTML.split(" ").length)
-}  
-
-//A button “Like” for the user to like a quote. Hint : add a new key to each object that will represent the number of “likes”.
-
-
-likeButton.addEventListener('click', toLike)
-function toLike() {
-  likesResult.value ++;
-  likesResult.innerText = likesResult.value
-}  
-
-
-let addingForm = document.getElementById('addQuote')
-let quoteInput = document.getElementById('quote')
-let authorInput = document.getElementById('author')
-
-const addNewOne = (e) => {
-  e.preventDefault()
-  let quote = {
-    id : quotes.length,
-    author: authorInput.value,
-    quote : quoteInput.value
-  }
-  quotes.push(quote)
-}
-
-addingForm.addEventListener('submit', addNewOne);
-
-let searchForm = document.getElementById('search')
-let nextButton = document.getElementById('next')
-let previousButton = document.getElementById('previous')
-
-
-searchForm.addEventListener('submit', byAuthor)
-
-let nameOfAuthor = document.getElementById('name')
-let place = document.getElementById('quotesbyauthor')
-function byAuthor(e) {
-  let n = 0;
-  const spesific = quotes.filter(each => each.author == nameOfAuthor.value);
-    let newDisplay = document.createElement('div')
-    newDisplay.innerHTML = spesific[n].quote;
-    console.log(spesific.length)
-    place.appendChild(newDisplay)
-    nextButton.addEventListener('click', function() {
-      if (n < spesific.length-1) {
-      n +=1;
-      newDisplay.innerHTML = spesific[n].quote;
-      }
+      this.imageCard = document.createElement('img')
+      this.imageCard.className = "card-img"
+      this.imageCard.src = this.image;
       
-    })
-    previousButton.addEventListener('click', function() {
-      if (n > 0) {
-      n-=1
-      newDisplay.innerHTML = spesific[n].quote;
-      }
-    })
-  e.preventDefault()
-}
+      this.cardTitle = document.createElement('h3')
+      this.cardTitle.textContent = this.name;
+
+      this.cardSubTitle = document.createElement('h5')
+      this.cardSubTitle.textContent = this.username;
+
+      this.emailTitle = document.createElement('h5')
+      this.emailTitle.textContent = this.email;
+
+      this.robotCard.append(this.imageCard)
+      this.robotCard.append(this.cardTitle)
+      this.robotCard.append(this.cardSubTitle)
+      this.robotCard.append(this.emailTitle)
+
+      space.append(this.robotCard)
+      allRobots.push(this)
+    }
+
+    show(){
+      this.robotCard.classList.remove('hidden')
+    }
+    hide(){
+      this.robotCard.classList.add('hidden')
+
+    }
+  }
+
+    robots.forEach(robot => {
+      let makeRobot = new Robot(robot.id, robot.name, robot.username, robot.email, robot.image)
+      makeRobot.createRobotCard()
+    });
+
+  let searchInput = document.getElementById('search')
+  searchInput.addEventListener('input', makeSearch)
+
+  function makeSearch(element) {
+    allRobots.forEach(robot => robot.name.toLowerCase().includes(element.target.value.toLowerCase()) ? robot.show() : robot.hide())
+
+  }
